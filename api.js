7bb82bea166966ef49cab8830e923a636cf8d2fa
Zeeshan <53146394+ElectraPro-pk@users.noreply.github.com>
2021-09-23 01:16:55 +0300

    Update api.js

--- api.js
+++ api.js
@@ -22,7 +22,7 @@ app.get('/items',(req,res) => {
     sql = "SELECT * FROM items ORDER BY serialNo"
     dbConn.query(sql,function(err,response){
         if(err){
-            res.sendStatus(505).send(err).end();
+             res.json({"status":503});
         }
         res.json({"data":response})
     })
@@ -32,16 +32,16 @@ app.get('/new-item/:id,:product,:category,:price',(req,res) => {
     sql = "INSERT INTO items VALUES('"+req.params.id+"','"+req.params.product+"','"+req.params.category+"','"+req.params.price+"')"
     dbConn.query(sql,function(err,response){
         if(err){
-            res.send("NA").end()
+             res.json({"status":503});
         }
-        res.sendStatus(200).end();
+        res.json({"status":200});
     })
 })
 app.get('/search-item/:query',(req,res) => {
     sql = "SELECT * from items where product LIKE '%"+req.params.query.toLowerCase()+"%' or category LIKE '%"+req.params.query.toLowerCase()+"%' order by serialNo"
     dbConn.query(sql,function(err,response){
         if(err){
-            res.sendStatus(505).send(err).end();
+           res.json({"status":503});
         }
         res.json({"data":response})
     })
@@ -50,18 +50,18 @@ app.get('/delete-item/:id',(req,res) => {
     sql = "DELETE FROM ITEMS where serialNo = '"+req.params.id+"'"
     dbConn.query(sql,function(err,response){
         if(err){
-            res.send("NA").end()
+              res.json({"status":503});
         }
-        res.sendStatus(200).end();
+        res.json({"status":200});
     })
 })
 app.get('/update-item/:id,:product,:category,:price',(req,res) => {
     sql ="UPDATE items SET product = '"+req.params.product+"',category = '"+req.params.category+"', price = '"+req.params.price+"' where serialNo = '"+req.params.id+"'"
     dbConn.query(sql,function(err,response){
         if(err){
-            res.send("NA").end()
+              res.json({"status":503});
         }
-        res.sendStatus(200).end();
+       res.json({"status":200});
     })
 })
 
